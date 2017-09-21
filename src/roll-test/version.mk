56512ef8c01ab3a67d80b2d7ed60ece0e204aca1
Jerry Greenberg <jpg@sdsc.edu>
2017-09-20 17:46:55 -0700

    APS-546 addition of cuda test

--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,6 +1,10 @@
 NAME       = sdsc-abinit-roll-test
-VERSION    = 1
+VERSION    = 2
 RELEASE    = 0
-PKGROOT    = /root/rolltests
+PKGROOT    = /home/jpg/cometscratch/abinit-roll/src/roll-test/test
+
+ifneq ("$(ROLLOPTS)", "$(subst cuda=,,$(ROLLOPTS))")
+  CUDAVER = $(subst cuda=,,$(filter cuda=%,$(ROLLOPTS)))
+endif
 
 RPM.EXTRAS = AutoReq:No
